d728a16a45a957481da0df2cef301d4eb073f7fc
jan88611 <54337963+jan88611@users.noreply.github.com>
2019-09-04 10:39:47 +0800

    Create gamesetting.py

---
+++ gamesetting.py
@@ -0,0 +1,20 @@
+# GAME SETTINGS
+WHITE     = (255, 255, 255)
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+GREEN     = (  0, 255,   0)
+PINK      = (255, 192, 203)
+DARKGRAY  = ( 40,  40,  40)
+
+FPS = 60
+WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 600
+CAPTION = "哪吒"
+
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = - 0.12
+
+#game_folder = os.path.dirname(__file__)
+#img_folder = os.path.join(game_folder, "image")
+
+#player_image = os.path.join(img_folder, "哪吒.gif").convert()
